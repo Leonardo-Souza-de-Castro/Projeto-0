67a4687fb9f917f754305ccc4469d5b193420fd9
LEONARDO SOUZA DE CASTRO <uniflcastro@feinet.fei.edu.br>
2024-03-19 20:47:21 -0300

    Arquivo .c e .h de tarefas funcionando e já implementado no loop

---
+++ tarefas.c
@@ -0,0 +1,27 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+int Criar_tarefa(Tarefa tarefas[], int *posicao){
+    printf("Funcao de criar tarefa\n");
+    return 0;
+};
+
+int Deletar_tarefa(Tarefa tarefas[], int *posicao){
+    printf("Funcao de deletar tarefa\n");
+    return 0;
+};
+
+int Listar_tarefa(Tarefa tarefas[], int posicao){
+    printf("Funcao de listar tarefa\n");
+    return 0;
+};
+
+int Salvar(Tarefa tarefas[], int total, int posicao){
+    printf("Funcao de salvar tarefa\n");
+    return 0;
+};
+
+int Carregar_tarefa(Tarefa tarefas[], int total, int posicao){
+    printf("Funcao de carregar tarefa\n");
+    return 0;
+};
